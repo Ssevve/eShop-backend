daec33c7fecafa5a471220771f5548bff5662f5b
Seweryn Świrgoń <ssevve@gmail.com>
2023-07-12 17:33:34 +0200

    Respond with 401 if token is not valid

---
+++ src/lib/firebaseErrors.ts
@@ -0,0 +1,5 @@
+const FirebaseErrors = {
+  ExpiredToken: 'auth/id-token-expired',
+} as const;
+
+export default FirebaseErrors;
--- src/middleware/ensureAuth.ts
+++ src/middleware/ensureAuth.ts
@@ -1,18 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
 import admin from '../firebaseConfig';
 import { findUserByFirebaseId } from '../api/users/users.service';
+import FirebaseErrors from '../lib/firebaseErrors';
 
 async function ensureAuth(req: Request, res: Response, next: NextFunction) {
-  const firebaseToken = req.headers.authorization?.split(' ')[1];
-  if (!firebaseToken) return res.sendStatus(401);
-
-  const firebaseUser = await admin.auth.verifyIdToken(firebaseToken);
-  if (!firebaseUser) return res.sendStatus(401);
-
-  const user = await findUserByFirebaseId(firebaseUser.uid);
-
-  req.user = user;
-  next();
+  try {
+    const firebaseToken = req.headers.authorization?.split(' ')[1];
+    if (!firebaseToken) return res.sendStatus(401);
+  
+    const firebaseUser = await admin.auth.verifyIdToken(firebaseToken);
+    if (!firebaseUser) return res.sendStatus(401);
+  
+    const user = await findUserByFirebaseId(firebaseUser.uid);
+  
+    req.user = user;
+    next();
+  } catch (error) {
+    if (error.code === FirebaseErrors.ExpiredToken) {
+      return res.status(401).json({ message: 'Invalid token.' });
+    }
+    next(error);
+  }
 }
 
 export default ensureAuth;
