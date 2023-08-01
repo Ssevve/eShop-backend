6e517ccfd7a2ec1b8ad2b34674e5416b7b01533d
Seweryn Świrgoń <ssevve@gmail.com>
2023-08-01 19:07:40 +0200

    Allow cors credentials

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,12 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
+  
+app.use(cors({
+  credentials: true,
+  origin: process.env.CORS_ORIGIN,
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
